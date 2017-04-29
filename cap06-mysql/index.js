371cfa22ffd562b1487f9157d33b8344f23afdff
Ricardo Lecheta <rlecheta@livetouch.com.br>
2017-04-29 18:47:08 -0300

    ok

--- cap06-mysql/index.js
+++ cap06-mysql/index.js
@@ -3,41 +3,39 @@ var http = require('http');
 var url  = require('url')
 var mysql      = require('mysql');
 
-// Retorna os carros para a tela
+// Retorna o JSON de uma lista de carros.
 function getCarros(response,tipo) {
 	// Faz a leitura do arquivo de forma assíncrona
 	// Cria a conexão com MySQL
-		var connection = mysql.createConnection({
-		  host     : 'localhost',
-		  user     : 'livro',
-		  password : 'livro123',
-		  database : 'livro'
-		});
+	var connection = mysql.createConnection({
+	  host     : 'localhost',
+	  user     : 'livro',
+	  password : 'livro123',
+	  database : 'livro'
+	});
 
-		// Conecta no banco de dados	
-		connection.connect();
+	// Conecta no banco de dados	
+	connection.connect();
 
-		// Cria uma consulta
-		connection.query("select id,nome,tipo from carro where tipo = '" + tipo + "'", function (error, results, fields) {
-		  if (error) throw error;
-		  
-		  // Aqui quero fazer um for e imprimir mas nao consigo
+	// Cria uma consulta
+	connection.query("select id,nome,tipo from carro where tipo = '" + tipo + "'", function (error, results, fields) {
+	  if (error) throw error;
+	  
+	  // Aqui quero fazer um for e imprimir mas nao consigo
 
-		  // Converte o array de resultados para JSON
-		  var json = JSON.stringify(results)
-		  
-		  // Envia o JSON como resposta
-		  response.end(json)
-		});
+	  // Converte o array de resultados para JSON
+	  var json = JSON.stringify(results)
+	  
+	  // Envia o JSON como resposta
+	  response.end(json)
+	});
 
-		// Fecha a conexão.
-		connection.end();
+	// Fecha a conexão.
+	connection.end();
 }
 
 // Função de callback para o servidor HTTP
 function callback(request, response) {
-	// Cabeçalho (header) com o tipo da resposta + UTF-8 como charset
-	// response.writeHead(200, {"Content-Type": "application/json; charset=utf-8"});
 	// Faz o parser da URL separando o caminho (path)
 	var parts = url.parse(request.url);
 	var path = parts.path;
