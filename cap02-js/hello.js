b2359acf5bc3a24a7821479d2941f3e4db581dc9
Ricardo Lecheta <rlecheta@livetouch.com.br>
2017-05-01 15:22:49 -0300

    ok

--- cap02-js/hello.js
+++ cap02-js/hello.js
@@ -53,4 +53,68 @@ function testeMap() {
 	console.log("Nome completo:", map["nome"], map["sobrenome"])
 
 }
-testeMap()
+// testeMap()
+
+/*
+function objetos1() {
+	var pessoa = Object()
+	pessoa.nome = "Ricardo";
+	pessoa.hello = function() {
+		return "Hello Pessoa!";
+	}
+	console.log(pessoa);
+	console.log(pessoa.nome);
+	console.log(pessoa.hello());
+}
+objetos1();
+*/
+
+/*
+function objetos2() {
+	var pessoa = {
+		nome : 'Ricardo',
+		hello: function() {
+			return "Hello Pessoa!";
+		}
+	}
+	console.log(pessoa);
+	console.log(pessoa.nome);
+	console.log(pessoa.hello());
+}
+objetos2();
+*/
+
+/*
+function Pessoa() {
+		this.nome = "Ricardo";
+		this.hello = function() {
+			return "Hello Pessoa!";
+		}
+}
+function objetos3() {
+	var pessoa = new Pessoa();
+	console.log(pessoa);
+	console.log(pessoa.nome);
+	console.log(pessoa.hello());
+}
+objetos3();
+*/
+
+class Pessoa {
+	constructor() {
+		this.nome = "Ricardo";
+	}
+	static hello() {
+		return "Hello Pessoa!";
+	}
+
+}
+
+function objetos4() {
+	var pessoa = new Pessoa();
+	console.log(pessoa);
+	console.log(pessoa.nome);
+	console.log(Pessoa.hello());
+}
+objetos4();
+
---
+++ cap02-js/teste.js
@@ -0,0 +1,3 @@
+setTimeout(function(){
+	console.log("ok");
+},1);
