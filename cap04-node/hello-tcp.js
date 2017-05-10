5b22ac756b90d0fa41fc282714c6f54a67e2ddd4
Ricardo Lecheta <rlecheta@livetouch.com.br>
2017-05-10 14:40:23 -0300

    ok

--- cap04-node/hello-tcp.js
+++ cap04-node/hello-tcp.js
@@ -1,18 +1,15 @@
 // Carrega o módulo net
 var net = require('net');
-
+// Contador para testes
+var count = 1;
 // Cria o servidor TCP
 var server = net.createServer(function (socket) {
-	
 	// Mensagem de log quando algum cliente conectar
 	console.log("Cliente conectou do IP: " + socket.remoteAddress);
-
 	// Escreve a resposta e termina a conexão do cliente.
-	socket.end("Hello World TCP!\n");
+	socket.end("Hello World TCP: " + (count++) + "\n");
 });
-
 // Inicia o servidor
 server.listen(3000, "localhost");
-
 // Mensagem ao iniciar o servidor
-console.log("Servidor iniciado...");
+console.log("TCP server listening on port 3000 at localhost.");
